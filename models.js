80265f42f6d5f299679d0d4a3b9fa991657e115c
GuiEloi <gcunhaeloi@gmail.com>
2017-03-15 14:20:49 +1100

    Adjust in the initial script

--- models.js
+++ models.js
@@ -1,4 +1,7 @@
+exports.Category = require('./models/category');
+exports.Chat = require('./models/chat');
+exports.Company = require('./models/company');
+exports.Faq = require('./models/faq');
+exports.Iframe = require('./models/iframe');
+exports.Lead = require('./models/lead');
 exports.User = require('./models/user');
-exports.User = require('./models/category');
-exports.User = require('./models/iframe');
-exports.User = require('./models/company');
