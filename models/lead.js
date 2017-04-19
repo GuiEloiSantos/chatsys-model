b98654dc1827dee5cc5c2916a0695d6d86b7a336
GuiEloi <gcunhaeloi@gmail.com>
2017-04-18 22:51:43 -0300

    Modification in the company and user model

--- models/lead.js
+++ models/lead.js
@@ -29,23 +29,31 @@ var lead = new Schema(
 lead.static({
     newLead: function (company_id, chat_id, notes, name, email, phone, date, custom_fields) {
         var Lead = this.model('Lead');
-        var lead= new Lead();
-        var situation = '';
-        if(!name && !email && !phone){
-            situation = 'SEE TRANSCRIPT';
-        }
-        lead.set({
-            company_id: company_id,
-            chat_id: chat_id,
-            notes: notes,
-            name: name,
-            email: email,
-            phone: phone,
-            date: date,
-            situation: situation,
-            custom_fields: custom_fields
+        Lead.getLeadByChatId(chat_id).exec().then(function (result) {
+           if(result){
+               return result.save();
+           }else {
+               var lead= new Lead();
+               var situation = '';
+               if(!name && !email && !phone){
+                   situation = 'SEE TRANSCRIPT';
+               }
+               lead.set({
+                   company_id: company_id,
+                   chat_id: chat_id,
+                   notes: notes,
+                   name: name,
+                   email: email,
+                   phone: phone,
+                   date: date,
+                   situation: situation,
+                   custom_fields: custom_fields
+               });
+               return lead.save();
+           }
         });
-        return lead.save();
+
+
     },
     getLeadByCompany: function (company_id) {
         var Lead = this.model('Lead');
