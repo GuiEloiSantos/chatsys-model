0e55c87a6cc451dc38de1fef3cec04bf35aba125
GuiEloi <gcunhaeloi@gmail.com>
2017-07-02 10:11:19 -0300

    Modification in the company and user model

---
+++ models/plans.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var plan = new Schema(
+    {
+        name: String,
+        sale_name: String,
+        code: {type: String, unique: true, index: true},
+        description: String,
+        addOns: {type: Array, default: []},
+        price: {type: Number, default: 99},
+        lead_limit: {type: Number, default: 0},
+        chat_limit: {type: Number, default: 0},
+        lead_price: {type: Number, default: 15},
+        lead_price: {type: Number, default: 5}
+    },
+    {
+        timestamps: true,
+        versionKey: false
+    }
+);
+
+plan.static({
+
+    newPlan: function (name, code, description, addOns, price, lead_limit, chat_limit, lead_price, chat_price) {
+        var Category = this.model('Category');
+        var category = new Category();
+        plan.set({
+            name: name,
+            code: code,
+            description: description,
+            addOns: addOns,
+            price: price,
+            lead_limit: lead_limit,
+            chat_limit: chat_limit,
+            lead_price: lead_price,
+            chat_price: chat_price
+        });
+        return plan.save();
+    }
+});
+module.exports = mongoose.model('Plan', plan);
