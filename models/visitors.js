f4c57ef370d7a3f84a5c0b0454c1401abb1446e0
GuiEloi <gcunhaeloi@gmail.com>
2017-10-29 15:44:02 -0200

    Adding visitor table

--- models/visitors.js
+++ models/visitors.js
@@ -4,12 +4,13 @@ let Schema = mongoose.Schema;
 
 let visitor = new Schema(
     {
+        visitor_id: {type: String, unique: true, index: true},
         code: {type: String, index: true},
         ip: String,
         website: String,
         location: String,
         date: {type: Date},
-        company_id: {type: Schema.Types.ObjectId, ref: 'company'}
+        company_id: {type: Schema.Types.ObjectId, ref: 'company', index: true}
     },
     {
         timestamps: true,
@@ -20,13 +21,19 @@ visitor.static({
     addNew: function (ip, code, website, location, company_id) {
         let Visitor = this.model('Visitor');
         let visitor = new Visitor();
+        let date = new Date();
+        visitor.set({visitor_id: code + company_id + date.getDate() + date.getMonth() + date.getFullYear()});
         visitor.set({code: code});
         visitor.set({ip: ip});
         visitor.set({website: website});
         visitor.set({company_id: company_id});
         visitor.set({location: location.join(' - ')});
-        visitor.set({date: new Date()});
-        return visitor.save();
+        visitor.set({date: date});
+        return visitor.save(function (err) {
+            if (err && err.code === 11000) {
+                return Promise.resolve(err);
+            }
+        });
     }
 });
 module.exports = mongoose.model('Visitor', visitor);
